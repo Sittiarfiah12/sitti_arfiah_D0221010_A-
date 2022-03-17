fbdbb8e458321d358557b40a2ebe8f37dfb8074c
SITTI ARFIAH <99633921+Sittiarfiah12@users.noreply.github.com>
2022-03-17 15:06:25 +0800

    Create day27_java_biglnteger.java

---
+++ day27_java_biglnteger.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+import java.util.*;
+
+import java.text.*;
+
+import java.math.*;
+
+import java.util.regex.*;
+
+Penyelesaian kelas awam {
+
+    public static void main(String[] args) {
+
+        Pengimbas sc = Pengimbas baharu(System.in);
+
+        Rentetan a = sc.next();
+
+        Rentetan b = sc.next();
+
+        BigInteger A = BigInteger(a);
+
+        Integer Besar B = Integer Besar baharu(b);
+
+        Integer Besar C = A.tambah(B);
+
+        Integer Besar D = A.darab(B);
+
+        System.out.println(C);//hasil penjumlahan
+
+        System.out.println(D);//hasil perkalian
+
+    }
+
+},
